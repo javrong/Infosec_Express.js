983b848d2ad51f215b78813695b2ba23afd23a6c
javrong <sunumd@yahoo.com>
2021-12-13 00:11:40 +0700

    initial commit

---
+++ myApp.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const bcrypt = require('bcrypt');
+const ninetyDaysInSeconds = 90*24*60*60;
+
+app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen())
+app.use(helmet.hsts({
+  maxAge: 90*24*60*60, 
+  force: 'true'
+  }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
+
+module.exports = app;
+
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
